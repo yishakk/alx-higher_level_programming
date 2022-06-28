eb05074422c6dc298f73aeae17279a7f4b74187e
yishakk <yishakkibru@gmail.com>
2022-06-28 11:47:39 -0700

    5-print_comb2.py

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -4,8 +4,3 @@ for numbers in range(0, 100):
         print("{:02d}, ".format(numbers), end='')
     else:
         print("{:02d}".format(numbers))
-© 2022 GitHub, Inc.
-Footer navigation
-Terms
-Privacy
-Security
