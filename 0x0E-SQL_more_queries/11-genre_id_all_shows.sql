2527670d447129532b55b12df0ed81ac5269c62b
yishakk <yishakkibru@gmail.com>
2022-09-07 16:03:55 +0300

    0x0E-SQL_more_queries

---
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,5 @@
+-- Script that show join two tables data to show correlation.
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title, tv_show_genres.genre_id;
